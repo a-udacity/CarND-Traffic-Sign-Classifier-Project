19f7916dc742f15a142a6bc865a65fc6d98f29ac
Aziz Hararwala <ahararwala@pillartechnology.com>
2017-01-08 08:17:57 -0500

    Successful run of LNet as per Lab

--- Traffic_Sign_Classifier.py
+++ Traffic_Sign_Classifier.py
@@ -21,7 +21,7 @@
 # Load pickled data
 import pickle
 
-# TODO: Fill this in based on where you saved the training and testing data
+# Fill this in based on where you saved the training and testing data
 
 training_file = 'traffic-signs-data/train.p'
 testing_file = 'traffic-signs-data/test.p'
@@ -66,16 +66,16 @@ print("Test Set:       {} samples".format(len(X_test)))
 ### Replace each question mark with the appropriate value.
 
 
-# TODO: Number of training examples
+# Number of training examples
 n_train = len(X_train)
 
-# TODO: Number of testing examples.
+# Number of testing examples.
 n_test = len(X_test)
 
-# TODO: What's the shape of an traffic sign image?
+# What's the shape of an traffic sign image?
 image_shape = X_train.shape[0]
 
-# TODO: How many unique classes/labels there are in the dataset.
+# How many unique classes/labels there are in the dataset.
 n_classes = 43
 
 print("Number of training examples =", n_train)
@@ -105,13 +105,141 @@ import matplotlib.pyplot as plt
 
 #get_ipython().magic('matplotlib inline')
 
-index = random.randint(0, len(X_train))
-image = X_train[index].squeeze()
+def plot_images():
+    index = random.randint(0, len(X_train))
+    image = X_train[index].squeeze()
+    plt.figure(figsize=(1, 1))
+    plt.imshow(image)
+    print(y_train[index])
 
-plt.figure(figsize=(1, 1))
-plt.imshow(image)
-print(y_train[index])
 
+plot_images()
+plot_images()
+plot_images()
+plot_images()
+plot_images()
+
+
+from sklearn.utils import shuffle
+
+X_train, y_train = shuffle(X_train, y_train)
+
+# ## Setup TensorFlow
+# The `EPOCH` and `BATCH_SIZE` values affect the training speed and model accuracy.
+#
+# You do not need to modify this section.
+
+# In[8]:
+
+import tensorflow as tf
+
+EPOCHS = 10
+BATCH_SIZE = 128
+
+from tensorflow.contrib.layers import flatten
+
+
+def LeNet(x):
+    # Hyperparameters
+    mu = 0
+    sigma = 0.1
+
+    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean=mu, stddev=sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    # Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+
+    # Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # Flatten. Input = 5x5x16. Output = 400.
+    fc0 = flatten(conv2)
+
+    # Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
+
+    # Activation.
+    fc1 = tf.nn.relu(fc1)
+
+    # Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1, fc2_W) + fc2_b
+
+    # Activation.
+    fc2 = tf.nn.relu(fc2)
+
+    # Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_W = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=mu, stddev=sigma))
+    fc3_b = tf.Variable(tf.zeros(43))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+
+    return logits
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 43)
+
+
+rate = 0.001
+
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=rate)
+training_operation = optimizer.minimize(loss_operation)
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {} ...".format(i + 1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+
+    saver.save(sess, 'lenet')
+    print("Model saved")
 # ----
 # 
 # ## Step 2: Design and Test a Model Architecture
